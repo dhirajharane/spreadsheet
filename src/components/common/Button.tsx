81d076f27ae0e76646789a4e507c70fd429191a4
dhirajharane <dhirajharane@gmail.com>
2025-07-15 20:21:30 +0530

    fixing the button

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,4 +1,4 @@
-import type { FC, MouseEvent } from 'react';
+import type { FC } from 'react';
 
 interface ButtonProps {
   children: React.ReactNode;
@@ -14,7 +14,7 @@ export const Button: FC<ButtonProps> = ({ children, onClick, variant = 'primary'
       ? 'bg-[#0066FF] text-white hover:bg-[#0055CC]'
       : 'bg-[#F5F6F5] text-[#1A1D1F] hover:bg-[#E8ECEF]';
 
-  const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
+  const handleClick = () => {
     console.log(`${children} button clicked`);
     onClick();
   };
